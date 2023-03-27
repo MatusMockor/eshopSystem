420846d5c8681f9a777c14031c11923ded3b6bb0
Matúš Močkor <codex611@gmail.com>
2023-03-27 22:56:20 +0200

    ADDED: fillable properties

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $name
+ * @property string $slug
+ * @property string $description
+ * @property double $price
+ */
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+    ];
 }
