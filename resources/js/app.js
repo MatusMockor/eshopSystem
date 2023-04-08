4f29d6469791f7f71d98451a3e6baa2a11962ff4
Matúš Močkor <codex611@gmail.com>
2023-04-08 13:35:43 +0200

    REMOVED: imported test component - not exist

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
 import {createApp} from "vue";
-import TestComponent from "./components/admin/TestComponent.vue";
 // import Alpine from 'alpinejs';
 
 // window.Alpine = Alpine;
