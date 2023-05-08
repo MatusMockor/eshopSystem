f04d96f56a932d9c71bafcab1b4df9229cd6b80c
Matúš Močkor <codex611@gmail.com>
2023-05-08 16:53:09 +0200

    CHANGED: use helper user function in header.blade.php

--- resources/views/dashboard/_inc/header.blade.php
+++ resources/views/dashboard/_inc/header.blade.php
@@ -36,10 +36,10 @@
                                 id="dropdown-user">
                             <div class="px-4 py-3" role="none">
                                 <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    {{auth()->user()->present()->fullName}}
+                                    {{user()?->present()->fullName}}
                                 </p>
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    neil.sims@flowbite.com
+                                    {{user()?->email}}
                                 </p>
                             </div>
                             <ul class="py-1" role="none">
