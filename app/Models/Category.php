9660348ffd9b2185aaf6a51ae3d265f4e76698c2
Matúš Močkor <codex611@gmail.com>
2023-05-18 18:11:26 +0200

    CHANGED: format category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,16 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Category
  *
- * @property string $name
- * @property string $slug
- * @property int $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @mixin \Eloquent
+ * @property string      $name
+ * @property string      $slug
+ * @property int         $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  */
 class Category extends Model
 {
