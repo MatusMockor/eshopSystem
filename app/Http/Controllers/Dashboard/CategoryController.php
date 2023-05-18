273cb47d403600aafce82089c6ca8f22cd61aded
Matúš Močkor <codex611@gmail.com>
2023-05-18 18:14:09 +0200

    ADDED: index method - paginate all categories

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-
+    public function index()
+    {
+        $categories = Category::simplePaginate();
+        return view('dashboard.category.index', [
+            'categories' => $categories
+        ]);
+    }
 }
