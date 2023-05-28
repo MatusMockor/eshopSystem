9bc0e03d0720b6c1329116427c86fe09c36467b2
Matúš Močkor <codex611@gmail.com>
2023-05-28 23:41:19 +0200

    ADDED: ckeditor configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 import {createApp} from "vue";
 import 'flowbite';
 import CategoryForm from "../components/dashboard/CategoryForm.vue";
+import ProductForm from "../components/dashboard/ProductForm.vue";
+import CKEditor from '@ckeditor/ckeditor5-vue';
 // import Alpine from 'alpinejs';
 
 // window.Alpine = Alpine;
@@ -10,5 +12,7 @@ import CategoryForm from "../components/dashboard/CategoryForm.vue";
 const app = createApp({});
 
 app.component('CategoryForm', CategoryForm);
+app.component('ProductForm', ProductForm);
 
+app.use(CKEditor);
 app.mount('#app');
