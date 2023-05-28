ec8dded9364261d8e87082470d52909cae997255
Matúš Močkor <codex611@gmail.com>
2023-05-28 12:11:27 +0200

    ADDED: dashboard link into pop up menu

--- resources/views/dashboard/_inc/header.blade.php
+++ resources/views/dashboard/_inc/header.blade.php
@@ -45,9 +45,10 @@
                             </div>
                             <ul class="py-1" role="none">
                                 <li>
-                                    <a href="#"
+                                    <a href="{{route('dashboard')}}"
                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                       role="menuitem">Dashboard</a>
+                                       role="menuitem">Dashboard
+                                    </a>
                                 </li>
                                 <li>
                                     <a href="#"
