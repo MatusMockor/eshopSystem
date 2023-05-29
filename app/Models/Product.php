4019be61eeb5cf7d3df886b4933b69d24adc3733
Matúš Močkor <codex611@gmail.com>
2023-05-29 20:21:02 +0200

    ADDED: statuses for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,10 +15,31 @@ class Product extends Model
 {
     use HasFactory;
 
+    // This status indicates that the product is active and displayed on the e-commerce website.
+    // Customers can view and purchase it.
+    const STATUS_ACTIVE = 'active';
+    // This status indicates that the product is inactive and not displayed on the e-commerce website.
+    // Customers cannot see or purchase it. This status is often used when you want to temporarily
+    // hide a product, such as due to unavailability, repairs, or updates.
+    const STATUS_INACTIVE = 'inactive';
+    // This status indicates that the product is currently sold out and cannot be purchased. It is displayed
+    // on the e-commerce website but without the option to add it to the cart. This status is commonly
+    // used to inform customers about the temporary unavailability of a product.
+    const STATUS_SOLD_OUT = 'sold_out';
+
     protected $fillable = [
         'name',
         'slug',
         'description',
         'price',
     ];
+
+    public static function getStatuses() : array
+    {
+        return [
+            self::STATUS_ACTIVE,
+            self::STATUS_INACTIVE,
+            self::STATUS_SOLD_OUT,
+        ];
+    }
 }
