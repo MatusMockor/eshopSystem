12e3a227ae0291191ef507c1671e9cf696dd8f90
Matúš Močkor <codex611@gmail.com>
2023-06-06 22:32:16 +0200

    CHANGED: images is array

--- resources/components/dashboard/ProductForm.vue
+++ resources/components/dashboard/ProductForm.vue
@@ -41,7 +41,7 @@
     </label>
     <input
         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-        id="multiple_files" type="file" accept="image/png, image/jpeg, image/jpg" multiple/>
+        id="multiple_files" name="images[]" type="file" accept="image/png, image/jpeg, image/jpg" multiple/>
     <button type="submit"
             class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-10">
         Save
