3cb567d0b5623d1cb3289e71837abcfb6cc37776
Matúš Močkor <codex611@gmail.com>
2023-06-10 13:33:38 +0200

    ADDED: possibility to show product edit page

---
+++ resources/views/dashboard/product/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('dashboard.master')
+
+@section('title', 'Add product')
+@section('content')
+    <form method="post" action="{{route('dashboard.products.update')}}" enctype="multipart/form-data">
+        @csrf
+        <product-form
+            :product="{{json_encode($product)}}"
+            :statuses="{{json_encode($statuses)}}"
+        >
+        </product-form>
+        {{--        <button type="button"--}}
+        {{--                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">--}}
+        {{--            Remove--}}
+        {{--        </button>--}}
+    </form>
+
+@endsection
--- routes/dashboard/product.php
+++ routes/dashboard/product.php
@@ -6,4 +6,5 @@ Route::group(['prefix' => 'products', 'middleware' => 'roles:admin'], function (
     Route::get('/', [ProductController::class, 'index'])->name('dashboard.products.index');
     Route::get('/create', [ProductController::class, 'create'])->name('dashboard.products.create');
     Route::post('/', [ProductController::class, 'store'])->name('dashboard.products.store');
+    Route::get('/{product}/edit', [ProductController::class, 'edit'])->name('dashboard.products.edit');
 });
