67cfdf7b0b76ea92b2b2b88d29c43ce24bbc55a1
Matúš Močkor <codex611@gmail.com>
2023-06-14 18:44:37 +0200

    CHANGED: code formatting

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends Controller
     public function create()
     {
         return view('dashboard.product.create', [
-            'statuses' => Product::getStatuses()
+            'statuses' => Product::getStatuses(),
         ]);
     }
 
