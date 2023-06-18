5e5edbada5302b9833dcbccb75d8f331d05595c6
Matúš Močkor <codex611@gmail.com>
2023-06-18 13:57:36 +0200

    ADDED: image_name, image_path, mime, ext, size, is_featured on image table / model

--- database/migrations/2023_05_31_195948_create_images_table.php
+++ database/migrations/2023_05_31_195948_create_images_table.php
@@ -4,16 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up() : void
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->morphs('imageable');
+            $table->string('image_name');
+            $table->string('image_path');
+            $table->string('mime');
+            $table->string('ext');
+            $table->integer('size');
+            $table->boolean('is_featured')->default(false);
             $table->timestamps();
         });
     }
@@ -21,7 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down() : void
     {
         Schema::dropIfExists('images');
     }
