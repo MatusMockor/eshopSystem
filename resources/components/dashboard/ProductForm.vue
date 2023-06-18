28bb7291804cd43367a91cbc4a73de2b580651d6
Matúš Močkor <codex611@gmail.com>
2023-06-18 21:17:59 +0200

    REMOVED: forgotten test text

--- resources/components/dashboard/ProductForm.vue
+++ resources/components/dashboard/ProductForm.vue
@@ -58,7 +58,7 @@
             class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-10">
         Save
     </button>
-    testik
+
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mt-10">
         <div v-for="image in images">
             <img class="h-auto max-w-full rounded-lg"
