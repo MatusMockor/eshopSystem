d415c1382889196f245e32974c3fe595fb9fe9f5
Matúš Močkor <codex611@gmail.com>
2023-06-18 19:30:49 +0200

    ADDED: test that user can upload image for product

--- tests/Feature/Product/ProductManagementTest.php
+++ tests/Feature/Product/ProductManagementTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Image;
 use App\Models\Product;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use function Pest\Laravel\delete;
 use function Pest\Laravel\patch;
 use function Pest\Laravel\post;
@@ -61,4 +64,27 @@ it('admin can delete product', function () {
 
     delete(route('dashboard.products.delete', $product));
     expect(Product::firstWhere('id', $productId))->toBeNull();
+});
+
+it('user can upload images for product', function () {
+    Storage::fake('local')->allDirectories();
+    login();
+
+    $product = Product::factory()->create();
+    $image = UploadedFile::fake()->image('test.png');
+
+    patch(route('dashboard.products.update', $product), [
+        'name'        => $product->name,
+        'status'      => $product->status,
+        'description' => $product->description,
+        'price'       => $product->price,
+        'quantity'    => $product->quantity,
+        'images'      => [
+            $image,
+        ],
+    ]);
+
+    /** @var Image $image */
+    $image = $product->images->first();
+    Storage::disk('local')->assertExists($image->image_path);
 });
