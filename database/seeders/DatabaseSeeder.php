ec6188af82f0d6a9a378f7e84d6860bb458302b8
Matúš Močkor <codex611@gmail.com>
2023-06-18 14:28:45 +0200

    CHANGED: import user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run() : void
     {
-        \App\Models\User::factory()->create();
+        User::factory()->create();
     }
 }
