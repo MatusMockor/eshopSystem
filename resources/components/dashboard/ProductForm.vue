543b4a91afde25ca6971fa8fe7a9e292c58786df
Matúš Močkor <codex611@gmail.com>
2023-06-19 20:11:12 +0200

    CHANGED: formatted code

--- resources/components/dashboard/ProductForm.vue
+++ resources/components/dashboard/ProductForm.vue
@@ -41,7 +41,8 @@
                     name="status"
                     class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
                     required>
-                <option v-for="category in categories" :value="category.id" :key="category.id" :selected="category === categoryId">
+                <option v-for="category in categories" :value="category.id" :key="category.id"
+                        :selected="category === categoryId">
                     {{ category.name }}
                 </option>
             </select>
