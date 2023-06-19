d770fa8f55289d524870f086eb23149bf001d3ed
Matúš Močkor <codex611@gmail.com>
2023-06-19 20:10:35 +0200

    FIXED: undefined property status

--- resources/components/dashboard/ProductForm.vue
+++ resources/components/dashboard/ProductForm.vue
@@ -25,7 +25,7 @@
         <div class="relative z-0 w-full mb-6 group">
             <label for="statuses" class="sr-only">Select status</label>
             <select id="statuses"
-                    v-model="status"
+                    v-model="productStatus"
                     name="status"
                     class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
                     required>
