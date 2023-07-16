5fe7b2cce6e6b14bdc383f5b84584a001a8ea251
Matúš Močkor <codex611@gmail.com>
2023-07-16 21:53:23 +0200

    ADDED: route file for pages and route for index

---
+++ routes/dashboard/page.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\Dashboard\PageController;
+
+Route::group(['prefix' => 'pages', 'middleware' => 'roles:admin'], function () {
+    Route::get('/', [PageController::class, 'index'])->name('dashboard.page.index');
+});
