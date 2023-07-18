fb892b6b7144a6ddf27bde929bf12ce133bf80f8
Matúš Močkor <codex611@gmail.com>
2023-07-18 20:43:12 +0200

    CHANGED: move observers into dashboard directory

--- app/Providers/ObserverServiceProvider.php
+++ app/Providers/ObserverServiceProvider.php
@@ -4,8 +4,8 @@ namespace App\Providers;
 
 use App\Models\Category;
 use App\Models\Product;
-use App\Observers\CategoryObserver;
-use App\Observers\ProductObserver;
+use App\Observers\Dashboard\CategoryObserver;
+use App\Observers\Dashboard\ProductObserver;
 use Illuminate\Support\ServiceProvider;
 
 class ObserverServiceProvider extends ServiceProvider
