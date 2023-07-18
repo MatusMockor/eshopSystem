5557d1d615710212bb6b0b921647862750158247
Matúš Močkor <codex611@gmail.com>
2023-07-18 23:25:33 +0200

    ADDED: route for creating page

--- routes/dashboard/page.php
+++ routes/dashboard/page.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\Dashboard\PageController;
 
 Route::group(['prefix' => 'pages', 'middleware' => 'roles:admin'], function () {
     Route::get('/', [PageController::class, 'index'])->name('dashboard.pages.index');
+    Route::get('/create', [PageController::class, 'create'])->name('dashboard.pages.create');
 });
