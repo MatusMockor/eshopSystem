f555472dc527362ce0aa41b1af1657fb30b51816
Matúš Močkor <codex611@gmail.com>
2023-07-19 21:47:05 +0200

    FIXED: route name

--- tests/Feature/Page/PageManagementTest.php
+++ tests/Feature/Page/PageManagementTest.php
@@ -6,10 +6,10 @@ use function Pest\Laravel\get;
 test('admin sees pages in dashboard', function () {
     login();
     $page = Page::factory()->create();
-    get(route('dashboard.page.index'))->assertStatus(200);
+    get(route('dashboard.pages.index'))->assertStatus(200);
 });
 
 test('the user cannot see the pages in dashboard', function () {
     $page = Page::factory()->create();
-    get(route('dashboard.page.index'))->assertStatus(302);
+    get(route('dashboard.pages.index'))->assertStatus(302);
 });
