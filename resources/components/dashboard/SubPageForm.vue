bc0d55fa28c9a2f7031a06c9d56f829c9091ce34
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:27:11 +0200

    REMOVED: unnecessary images from SubPage component

--- resources/components/dashboard/SubPageForm.vue
+++ resources/components/dashboard/SubPageForm.vue
@@ -14,16 +14,6 @@
   <div class="w-full mb-10 group mt-10">
     <ckeditor :editor="editor" v-model="body" :config="editorConfig" class="h-full"></ckeditor>
   </div>
-  <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="multiple_files">
-    Upload multiple files
-  </label>
-  <input
-      class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-      id="multiple_files" name="images[]" type="file" accept="image/png, image/jpeg, image/jpg" multiple/>
-  <button type="submit"
-          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-10">
-    Save
-  </button>
 </template>
 
 <script>
