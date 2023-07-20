0f8c8ae90d3b0d8f8f6f7e4cfc0450e4716007c6
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:33:46 +0200

    CHANGED: remove forgotten code

--- routes/dashboard/subPage.php
+++ routes/dashboard/subPage.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\Dashboard\SubPageController;
 Route::group(['prefix' => 'sub-page', 'middleware' => 'roles:admin'], function () {
     Route::get('/', [SubPageController::class, 'index'])->name('dashboard.subPages.index');
     Route::get('/create', [SubPageController::class, 'create'])->name('dashboard.subPages.create');
-    Route::post('/', [SubPageController::class, 'store'])->name('dashboard.subPages.store');
 });
