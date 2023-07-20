f33a7de0e3c211485dd6e4ed31a41a609e23581d
Matúš Močkor <codex611@gmail.com>
2023-07-20 22:06:27 +0200

    ADDED: testn admin can create sub page

--- tests/Feature/Page/SubPageManagementTest.php
+++ tests/Feature/Page/SubPageManagementTest.php
@@ -2,6 +2,8 @@
 
 use App\Models\SubPage;
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+use function PHPUnit\Framework\assertEquals;
 
 test('admin sees sub pages in dashboard', function () {
     login();
@@ -17,4 +19,14 @@ test('the user cannot see the sub pages in dashboard', function () {
 test('admin can visit create sub page form', function () {
     login();
     get(route('dashboard.subPages.create'))->assertStatus(200);
+});
+
+test('admin can create sub page', function () {
+    login();
+    post(route('dashboard.subPages.store', [
+        'name' => fake()->name,
+        'body' => fake()->text
+    ]))->assertStatus(302);
+
+    assertEquals(1, SubPage::count());
 });
