d220f7d854271512d4390b2b79ca0b85a3916cee
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:45:21 +0200

    ADDED: hidden button

--- resources/components/dashboard/SubPageForm.vue
+++ resources/components/dashboard/SubPageForm.vue
@@ -13,6 +13,7 @@
   </div>
   <div class="w-full mb-10 group mt-10">
     <ckeditor :editor="editor" v-model="body" :config="editorConfig" class="h-full"></ckeditor>
+    <input type="text" name="body" hidden="hidden" v-model="body">
   </div>
   <button type="submit"
           class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-10">
