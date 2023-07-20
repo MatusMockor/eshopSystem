59df629f002e208d9ef3bc6e4b001de8e501ceb4
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:05:14 +0200

    ADDED: test admin can visit create page form

--- tests/Feature/Page/PageManagementTest.php
+++ tests/Feature/Page/PageManagementTest.php
@@ -13,3 +13,8 @@ test('the user cannot see the pages in dashboard', function () {
     $page = Page::factory()->create();
     get(route('dashboard.pages.index'))->assertStatus(302);
 });
+
+test('admin can visit create page form', function () {
+    login();
+    get(route('dashboard.pages.create'))->assertStatus(200);
+});
