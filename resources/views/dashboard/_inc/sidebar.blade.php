c669f2ca7d816aded25a2bca27606c1b27b94d0c
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:33:04 +0200

    CHANGED: rename text Page in menu

--- resources/views/dashboard/_inc/sidebar.blade.php
+++ resources/views/dashboard/_inc/sidebar.blade.php
@@ -93,7 +93,7 @@
                         <path
                                 d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z"/>
                     </svg>
-                    <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Pages</span>
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Sub-Pages</span>
                     <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
                          xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
