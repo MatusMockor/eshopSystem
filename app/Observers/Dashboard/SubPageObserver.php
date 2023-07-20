eecdc1a8bee990864ae8f1827634f27e1ee8847f
Matúš Močkor <codex611@gmail.com>
2023-07-20 21:57:40 +0200

    CHANGED: rename sub page observer

---
+++ app/Observers/Dashboard/SubPageObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers\Dashboard;
+
+use App\Models\SubPage;
+
+class SubPageObserver
+{
+    public function creating(SubPage $page) : void
+    {
+        $page->slug = \Str::slug($page->name);
+    }
+
+    public function updating(SubPage $page) : void
+    {
+        $page->slug = \Str::slug($page->name);
+    }
+}
