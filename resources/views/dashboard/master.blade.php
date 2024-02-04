e662462caef5ae8f1e519c8e2df6b2527ef54e4c
Matúš Močkor <codex611@gmail.com>
2024-02-04 16:01:51 +0100

    bg gray for dashboard

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -1,5 +1,5 @@
 @include('dashboard._inc.head')
-<body id="app" class="bg-white dark:bg-gray-800">
+<body id="app" class="bg-white dark:bg-gray-900">
 @include('dashboard._inc.header')
 @include('dashboard._inc.sidebar')
 <div class="p-4 sm:ml-64">
