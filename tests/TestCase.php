68166c5f9c4c852c76a19943e6ac9812de621cb1
Matúš Močkor <codex611@gmail.com>
2024-02-14 21:57:03 +0100

    create setting for all tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use App\Models\Setting;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        Setting::factory()->create();
+    }
 }
