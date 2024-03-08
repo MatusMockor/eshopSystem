5ac86ff5e6f7acbce7ce04afc556af2ed6817c76
Matúš Močkor <codex611@gmail.com>
2024-03-08 17:28:44 +0100

    create table for product index page

--- resources/views/dashboard/product/index.blade.php
+++ resources/views/dashboard/product/index.blade.php
@@ -2,20 +2,64 @@
 
 @section('title', 'All products')
 @section('content')
+
     @if($products->count())
-        <div
-            class="mt-10 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-            @foreach($products as $product)
-                <a href="{{route('dashboard.products.edit', $product->id)}}"
-                   class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                    {{ $product->name }}
-                </a>
-            @endforeach
-            {{$products->links()}}
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Product name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Category
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Quantity
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Status
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($products as $product)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$product->name}}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{$product->category->name}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$product->quantity}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$product->price}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$product->status}}
+                        </td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="{{route('dashboard.products.edit', $product->id)}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
+        <br>
+            {{$products->links()}}
     @else()
         <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             <mark class="px-2 text-white bg-blue-600 rounded dark:bg-red-500">No products</mark>
         </h1>
     @endif
+    
 @endsection
