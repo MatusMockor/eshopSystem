11c6124df215eef98858961322f25f4d6b5acf38
Matúš Močkor <codex611@gmail.com>
2024-03-13 19:09:47 +0100

    refresh database before every test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use App\Models\Setting;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -12,6 +13,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
+        Artisan::call('migrate:fresh');
         Setting::factory()->create();
     }
 }
