0947d5ebc0fca7cee9bd260a58f8c7a1dd78997f
Matúš Močkor <codex611@gmail.com>
2024-03-13 19:11:38 +0100

    Implemented user authentication tests in Laravel

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -1,33 +1,42 @@
 <?php
 
+namespace Tests\Feature\Auth;
+
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Tests\TestCase;
 
-test('login screen can be rendered', function () {
-    $response = $this->get('/login');
+class AuthenticationTest extends TestCase
+{
+    public function test_login_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/login');
 
-    $response->assertStatus(200);
-});
+        $response->assertStatus(200);
+    }
 
-test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
+    public function test_users_can_authenticate_using_the_login_screen(): void
+    {
+        $user = User::factory()->create();
 
-    $response = $this->post('/login', [
-        'email'    => $user->email,
-        'password' => 'password',
-    ]);
+        $response = $this->post('/login', [
+            'email'    => $user->email,
+            'password' => 'password',
+        ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(RouteServiceProvider::HOME);
-});
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
 
-test('users can not authenticate with invalid password', function () {
-    $user = User::factory()->create();
+    public function test_users_can_not_authenticate_with_invalid_password(): void
+    {
+        $user = User::factory()->create();
 
-    $this->post('/login', [
-        'email'    => $user->email,
-        'password' => 'wrong-password',
-    ]);
+        $this->post('/login', [
+            'email'    => $user->email,
+            'password' => 'wrong-password',
+        ]);
 
-    $this->assertGuest();
-});
+        $this->assertGuest();
+    }
+}
