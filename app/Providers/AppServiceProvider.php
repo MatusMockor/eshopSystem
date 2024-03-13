51f99a8da47288cae605b312c95b025d8da59b78
Matúš Močkor <codex611@gmail.com>
2024-03-13 19:07:20 +0100

    set up database for testing = part of removing pest plugin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ParallelTesting::setUpTestDatabase(static fn () => Artisan::call('db:seed'));
     }
 }
